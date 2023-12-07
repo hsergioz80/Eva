247c6c7dcfe653f6d066b020feeb17aea1acb1e7
Sergio <hsergioz35@gmail.com>
2023-12-06 16:39:42 -0800

    Hello World

---
+++ Eva/EvaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvaApp.swift
+//  Eva
+//
+//  Created by sergio hernandez on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct EvaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
