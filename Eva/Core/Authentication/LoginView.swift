82998cc35e66444ab4cc9fd0da292a7ae6b024a5
Sergio <hsergioz35@gmail.com>
2023-12-06 18:13:47 -0800

    Email/Password, Sign In/Up

--- Eva/Core/Authentication/LoginView.swift
+++ Eva/Core/Authentication/LoginView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         NavigationStack{
             VStack{
@@ -17,10 +20,53 @@ struct LoginView: View {
                     .scaledToFill()
                     .frame(width: 100, height: 120)
                     .padding(.vertical, 32)
+                
                 //form fields
+                VStack(spacing: 24){
+                    InputView(text: $email,
+                              title: "Email Address",
+                              placeHolder: "name@email.com")
+                    .autocapitalization(.none)
+                    
+                    InputView(text: $password,
+                              title: "Password",
+                              placeHolder: "Enter your password",
+                              isSecureField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
                 //sign in
+                
+                Button{
+                    print("Log user in")
+                }label: {
+                    HStack{
+                        Text("SIGN IN")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32,
+                           height: 48)
+                }
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
                 Spacer()
+                    
                 //sign up
+                NavigationLink {
+                    
+                } label: {
+                    HStack(spacing:4){
+                        Text("Don't have an account? ")
+                        Text("Sign Up")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+
             }
         }
     }
