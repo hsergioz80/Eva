eb25fe5a743de42091de6fee32f85a837ed4b786
Sergio <hsergioz35@gmail.com>
2023-12-06 20:27:41 -0800

    ProfileView

--- Eva/Core/Profile/ProfileView.swift
+++ Eva/Core/Profile/ProfileView.swift
@@ -35,11 +35,34 @@ struct ProfileView: View {
             }
             
             Section("General"){
-                
+                HStack{
+                    SettingsRowView(imageName: "gear",
+                                    title: "Version",
+                                    tintColor: Color(.systemGray))
+                    
+                    Spacer()
+                    Text("1.0.0")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
             }
             
             Section("Account"){
+                Button{
+                    print("sign out")
+                }label: {
+                    SettingsRowView(imageName:"arrow.left.circle.fill",
+                                    title: "Sign Out",
+                                    tintColor: .red)
+                }
                 
+                Button{
+                    print("Delete Account")
+                }label: {
+                    SettingsRowView(imageName:"xmark.circle.fill",
+                                    title: "Delete Account",
+                                    tintColor: .red)
+                }
             }
         }
     }
