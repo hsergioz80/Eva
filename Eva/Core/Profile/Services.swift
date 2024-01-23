a44b6c1b6c20d2dc36b5252f18ed4717bd0f869d
Sergio <hsergioz35@gmail.com>
2024-01-22 17:23:44 -0800

    Thank you note

--- Eva/Core/Profile/Services.swift
+++ Eva/Core/Profile/Services.swift
@@ -11,30 +11,39 @@ import PassKit
 struct Services: View {
     @EnvironmentObject var viewModel: AuthViewModel
     @EnvironmentObject var cartManger: CartManger
-
+    
     var body: some View {
-                if let user = viewModel.currentUser{
-                    HStack(alignment: .top){
-                        VStack(alignment: .listRowSeparatorLeading){
-                            Text("Billing Contact: \n\n")
-                            Text("Pick up Info: \n\n\n")
-                            Text("Drop off Info: \n\n")
-                            Text("Total: $40").bold()
-                        }
-                        VStack(alignment: .listRowSeparatorLeading){
-                            Text(user.fullname + "\n" + user.email + "\n")
-                            Text(user.address)
-                            Text("\(user.PUDate) \n")
-                            Text(user.address)
-                            Text("\(user.DODate) \n")
-                        }
+        if cartManger.paymentSuccess{
+            Text("Thanks for your purchase :)")
+            NavigationLink(destination: ProfileView() ) {
+                Text("Continue")
+            }.navigationBarBackButtonHidden(true)
+
+        }
+        else{
+            if let user = viewModel.currentUser{
+                HStack(alignment: .top){
+                    VStack(alignment: .listRowSeparatorLeading){
+                        Text("Billing Contact: \n\n")
+                        Text("Pick up Info: \n\n\n")
+                        Text("Drop off Info: \n\n")
+                        Text("Total: $40").bold()
                     }
-                    VStack {
-                        PaymentButton(action: cartManger.pay)                        
+                    VStack(alignment: .listRowSeparatorLeading){
+                        Text(user.fullname + "\n" + user.email + "\n")
+                        Text(user.address)
+                        Text("\(user.PUDate) \n")
+                        Text(user.address)
+                        Text("\(user.DODate) \n")
                     }
+                }
+                VStack {
+                    PaymentButton(action: cartManger.pay)
                 }
             }
         }
+    }
+}
 
 #Preview {
     Services()
